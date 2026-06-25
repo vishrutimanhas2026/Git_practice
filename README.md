d68ede6db996ec81babcc7a2ce50d2237be04170
vishrutimanhas2026 <vishrutimanhas6005@gmail.com>
2026-06-25 14:46:39 +0530

    Created basic project structure

---
+++ README.md
@@ -0,0 +1,1 @@
+<!-- README.md: This explains what the project is, what it does, and how to use it. -->
---
+++ script.js
@@ -0,0 +1,1 @@
+// script.js: This adds behavior and interactivity to the page (clicks, logic, dynamic changes).
